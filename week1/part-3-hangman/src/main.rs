19d7a4b49f9ccb8d2525dc96f870baaf96f95fd1
Chen Anxue <46052474+cascades-sjtu@users.noreply.github.com>
2022-05-05 21:36:42 +0800

    finish part-3, not handles duplicate letter well

--- week1/part-3-hangman/src/main.rs
+++ week1/part-3-hangman/src/main.rs
@@ -17,6 +17,7 @@ use rand::Rng;
 use std::fs;
 use std::io;
 use std::io::Write;
+use std::iter::FromIterator;
 
 const NUM_INCORRECT_GUESSES: u32 = 5;
 const WORDS_PATH: &str = "words.txt";
@@ -34,7 +35,60 @@ fn main() {
     // secret_word by doing secret_word_chars[i].
     let secret_word_chars: Vec<char> = secret_word.chars().collect();
     // Uncomment for debugging:
-    // println!("random word: {}", secret_word);
+    println!("random word: {}", secret_word);
 
     // Your code here! :)
+    println!("Welcome to CS110L Hangman!");
+    let mut guess_word = vec!['-'; secret_word.len()];
+    let mut guessed_letters: Vec<char> = Vec::new();
+    let mut guesses = NUM_INCORRECT_GUESSES;
+
+    while guesses > 0 {
+        // print status
+        println!("The word so far is {}", String::from_iter(&guess_word));
+        println!(
+            "You have guessed the following letters: {}",
+            String::from_iter(&guessed_letters)
+        );
+        println!("You have {} guesses left", guesses);
+        print!("Please guess a letter: ");
+        // get input char
+        io::stdout().flush().expect("Error flushing out.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading line.");
+        // parse char
+        let guess_char = guess.chars().collect::<Vec<char>>()[0];
+        if guess_char.is_ascii_alphabetic() {
+            guessed_letters.push(guess_char);
+        }
+        // check if in secret word
+        match secret_word.find(guess_char) {
+            Some(loc) => {
+                guess_word[loc] = guess_char;
+            }
+            None => {
+                println!("Sorry, that letter is not in the word");
+                guesses -= 1;
+            }
+        }
+        print!("");
+        // check if successfully guess
+        if secret_word_chars == guess_word {
+            break;
+        }
+    }
+
+    match guesses {
+        0 => {
+            println!("Sorry, you ran out of guesses!");
+        }
+        _ => {
+            println!(
+                "Congratulations you guessed the secret word: {}",
+                secret_word
+            );
+        }
+    }
 }
