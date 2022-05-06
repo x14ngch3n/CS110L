38eec678f8fbe8485724e8f752db87057b887f1f
Chen Anxue <46052474+cascades-sjtu@users.noreply.github.com>
2022-05-06 09:32:54 +0800

    finish the simple rwc

--- week2/rwc/src/main.rs
+++ week2/rwc/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::process;
 
 fn main() {
@@ -8,5 +10,14 @@ fn main() {
         process::exit(1);
     }
     let filename = &args[1];
-    // Your code here :)
+    let file = File::open(filename).unwrap();
+    let (mut linen, mut wordn, mut charn) = (0, 0, 0);
+    for line in BufReader::new(file).lines() {
+        let line = line.unwrap();
+        linen += 1;
+        wordn += line.split_whitespace().count();
+        charn += line.chars().count();
+    }
+    println!("\tlines\twords\t characters");
+    println!("counts\t{}\t{}\t{}", linen, wordn, charn);
 }
