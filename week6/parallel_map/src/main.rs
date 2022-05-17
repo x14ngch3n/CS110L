3b32a4c621fc8dab823e6149441683c8a9375197
Chen Anxue <46052474+cascades-sjtu@users.noreply.github.com>
2022-05-17 13:32:09 +0800

    finish exercise-6

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -1,14 +1,55 @@
 use crossbeam_channel;
 use std::{thread, time};
 
-fn parallel_map<T, U, F>(mut input_vec: Vec<T>, num_threads: usize, f: F) -> Vec<U>
+fn parallel_map<T, U, F>(input_vec: Vec<T>, num_threads: usize, f: F) -> Vec<U>
 where
     F: FnOnce(T) -> U + Send + Copy + 'static,
     T: Send + 'static,
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
-    // TODO: implement parallel map!
+    output_vec.resize_with(input_vec.len(), Default::default);
+    // create two channels for sending data between worker threads and main thread
+    let (in_sender, in_receiver) = crossbeam_channel::unbounded();
+    let (out_sender, out_receiver) = crossbeam_channel::unbounded();
+    // spawn the worker threads
+    let mut handles = Vec::new();
+    for _ in 0..num_threads {
+        let out_sender_clone = out_sender.clone();
+        let in_receiver_clone = in_receiver.clone();
+        handles.push(thread::spawn(move || {
+            // wait for in_senders
+            while let Ok((idx, num)) = in_receiver_clone.recv() {
+                let res = f(num);
+                // send back to out_receivers
+                out_sender_clone
+                    .send((idx, res))
+                    .expect("There's no out_receiver");
+            }
+        }))
+    }
+
+    // send data to worker threads
+    for (idx, num) in input_vec.into_iter().enumerate() {
+        in_sender.send((idx, num)).expect("There's no in_receiver");
+    }
+
+    // tells in_receivers there's no in_senders
+    drop(in_sender);
+
+    // tells out_receivers there's no out_senders
+    drop(out_sender);
+
+    // receive result in order
+    while let Ok((idx, res)) = out_receiver.recv() {
+        output_vec[idx] = res;
+    }
+
+    // join all worker threads
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
     output_vec
 }
 
